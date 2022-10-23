3eaf2317154d8df3a6542284e0f581f5a7da990c
allmors <jesmora@protonmail.com>
2022-10-23 14:32:35 +0800

    fix error && update

--- public/global.js
+++ public/global.js
@@ -1,5 +1,7 @@
 /**
  * 这里是接口，一定要使用base64的格式，内部代码会自动转换
- * 接口：https://github.com/Hideipnetwork/hideipnetwork-admin.git
- * */
-export const url = 'aHR0cDovL2xvY2FsaG9zdDozMDAxLw==' 
+ * hideipnetwork-admin的部署，不能改变router结构，否则web项目报错 ！！！
+ * 这里的接口是admin部署后的完整接口
+*/
+const url = 'aHR0cDovL2xvY2FsaG9zdDozMDAx'
+window.localStorage.setItem('api', url)
