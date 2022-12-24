619982dc8212cdc5c0f170dc1d9098bfb2588708
allmors <jesmora@protonmail.com>
2022-12-24 17:02:29 +0800

    Publish V2

--- global.js
+++ global.js
@@ -2,8 +2,6 @@
  * 这里是接口，一定要使用base64的格式，内部代码会自动转换
  * hideipnetwork-admin的部署，不能改变router结构，否则web项目报错 ！！！
  * 这里的接口是admin部署后的完整接口
- * 
- * 如果使用napkin 这里就是napkin完整接口
 */
 
 const url = 'aHR0cDovLzEyNy4wLjAuMTozMDAxLw=='
