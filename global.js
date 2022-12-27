952b907b5685a134ed4c9c0f2e274369626268df
allmors <jesmora@protonmail.com>
2022-12-27 20:23:32 +0800

    add && fix errors

---
+++ global.js
@@ -0,0 +1,13 @@
+/**
+ * 这里是接口，一定要使用base64的格式，内部代码会自动转换
+ * hideipnetwork-admin的部署，不能改变router结构，否则web项目报错 ！！！
+ * 这里的接口是admin部署后的完整接口
+*/
+
+const HENTAPI = 'aHR0cDovL2xvY2FsaG9zdDozMDAxLw=='
+
+/**
+ * 密码设置
+ * false:表示api无效 无需部署admin，反之
+*/
+const HENTPWD = false
