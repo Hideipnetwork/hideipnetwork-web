7c5910e9c4bd4bd0a54e9da7e747bee41ac609e0
allmors <jesmora@protonmail.com>
2023-07-12 17:29:29 +0800

    up: 😴add changepwd

--- router/user.route.mjs
+++ router/user.route.mjs
@@ -4,5 +4,6 @@ import express from "express";
 const userRoute = express.Router();
 
 userRoute.post("/signin", userController.signin)
+userRoute.post("/changepwd",userController.changePwd)
 
 export default userRoute;
