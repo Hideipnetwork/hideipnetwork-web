7c3d4c2487d247e16902e6c1d7e3dad930d0b851
allmors <jesmora@protonmail.com>
2023-07-13 12:06:07 +0800

    update: up info

--- services/info.services.mjs
+++ services/info.services.mjs
@@ -2,7 +2,7 @@ import { infoModel } from "../models/info.model.mjs";
 
 class InfoServices {
     async getInfo() {
-        return await infoModel.findAll();
+        return await infoModel.findOne();
     }
 
     async addInfo({ title, password, bg, notify }) {
@@ -16,6 +16,25 @@ class InfoServices {
                 update_time: new Date()
             })
             return data
+        } catch (error) {
+
+        }
+    }
+
+    async editInfo(id, data) {
+        try {
+            const { title, password, bg, notify } = data;
+            const res = await infoModel.update({
+                title,
+                password,
+                bg,
+                notify,
+            }, {
+                where: {
+                    id
+                }
+            })
+            return res
         } catch (error) {
 
         }
