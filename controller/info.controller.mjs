8e911fd44a22711877f8e23f13299a0e6e31845f
allmors <jesmora@protonmail.com>
2023-07-13 18:20:25 +0800

    latest 😪

---
+++ controller/info.controller.mjs
@@ -0,0 +1,15 @@
+import infoServices from "../services/info.services.mjs";
+
+class InfoController {
+    async getInfo(req, res) {
+        const data = await infoServices.getInfo();
+        res.json({ code: 200, data })
+    }
+
+    async editInfo(req, res) {
+        const data = await infoServices.editInfo(req.body)
+        return res.json({ code: 200, data })
+    }
+}
+
+export default new InfoController();
