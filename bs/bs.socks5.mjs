75debdd72ad7fe16e745afef87e9f858c9ca45a2
allmors <jesmora@protonmail.com>
2023-07-14 18:07:37 +0800

    update

--- bs/bs.socks5.mjs
+++ bs/bs.socks5.mjs
@@ -1,20 +1,28 @@
 import { createBareServer } from '@tomphttp/bare-server-node';
 import { SocksProxyAgent } from 'socks-proxy-agent';
-import * as socksFun from '../utils/socks5.mjs';
+// import * as socksFun from '../utils/socks5.mjs';
 
-let bareServer = function () { };
-const data = await socksFun.getProxy();
-const random = Math.floor(Math.random() * data.length);
-const socks5 = data[random]
+import config from "../config/default.config.mjs";
 
-if (socks5 === undefined) {
-    bareServer = createBareServer('/bare/');
-} else {
-    const socksProxyAgent = new SocksProxyAgent(`socks://${socks5}`);
-    bareServer = createBareServer('/bare/', {
-        httpAgent: socksProxyAgent,
-        httpsAgent: socksProxyAgent,
-    });
-}
+// let bareServer = function () { };
+// const data = await socksFun.getProxy();
+// const random = Math.floor(Math.random() * data.length);
+// const socks5 = data[random]
+
+// if (socks5 === undefined) {
+//     bareServer = createBareServer('/bare/');
+// } else {
+//     const socksProxyAgent = new SocksProxyAgent(`socks://${socks5}`);
+//     bareServer = createBareServer('/bare/', {
+//         httpAgent: socksProxyAgent,
+//         httpsAgent: socksProxyAgent,
+//     });
+// }
+
+const socksProxyAgent = new SocksProxyAgent(`socks://${config.SOCKS5}`);
+const bareServer = createBareServer('/bare/', {
+    httpAgent: socksProxyAgent,
+    httpsAgent: socksProxyAgent,
+});
 
 export default bareServer;
