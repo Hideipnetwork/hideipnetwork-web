06a4b5bc5dc12724f75618bb758469b942103a45
allmors <jesmora@protonmail.com>
2023-07-20 14:52:56 +0800

    up

---
+++ public-233/server/server.config.js
@@ -0,0 +1,20 @@
+/**
+ * Just replace BAREUR
+ */
+
+let BAREURL = '/bare/';
+
+/**
+ * Non-developers, please modify without authorization, 98% report errors!
+*/
+self.__uv$config = {
+    prefix: `/aHR0cHM6Ly9naXRodWIuY29tL2hpZGVpcG5ldHdvcms=/`,
+    bare: `${BAREURL}`,
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/server/server.handler.js',
+    client: '/server/server.client.js',
+    bundle: '/server/server.bundle.js',
+    config: '/server/server.config.js',
+    sw: '/server/server.sw.js',
+};
