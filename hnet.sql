1b72ec0bdf0287c1232e8226453e7ec9a4532942
allmors <jesmora@protonmail.com>
2023-09-17 20:36:27 +0800

    update

--- hnet.sql
+++ hnet.sql
@@ -1,19 +1,3 @@
--- --------------------------------------------------------
--- 主机:                           mysql.sqlpub.com
--- 服务器版本:                        8.0.33 - MySQL Community Server - GPL
--- 服务器操作系统:                      Linux
--- HeidiSQL 版本:                  12.5.0.6677
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 -- 导出  表 fastify.hnet_info 结构
 CREATE TABLE IF NOT EXISTS `hnet_info` (
   `id` bigint NOT NULL AUTO_INCREMENT,
@@ -71,8 +55,3 @@ CREATE TABLE IF NOT EXISTS `hnet_user` (
 INSERT IGNORE INTO `hnet_user` (`id`, `username`, `password`, `isAdmin`, `create_time`, `update_time`) VALUES
 	(1, 'hnetadmin', 'hnetpassword', 1, '2023-07-14 16:12:35', '2023-07-14 16:12:35');
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
