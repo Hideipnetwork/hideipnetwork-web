d04349c13a274acd3a60518bc20dbd3c08b2dd5d
allmors <jesmora@protonmail.com>
2024-05-27 14:51:20 +0800

    add socks5

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,6 @@ import dotEnv from "dotenv"
 
 dotEnv.config()
 
-const server = new hnetServer(process.env.HOST, process.env.PORT, process.env.MODE)
+const server = new hnetServer({ HOST: process.env.HOST, PORT: process.env.PORT, MODE: process.env.MODE })
 
 server.start()
